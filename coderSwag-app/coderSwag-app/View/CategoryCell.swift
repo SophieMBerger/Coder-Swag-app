bb0516762844d7399d19e90e0af0e933a76ceb55
SophieMBerger <berger.m.sophie@gmail.com>
2018-08-11 14:40:53 +0200

    prototype cell

---
+++ coderSwag-app/coderSwag-app/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  coderSwag-app
+//
+//  Created by Sophie Berger on 11.08.18.
+//  Copyright © 2018 SophieMBerger. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
