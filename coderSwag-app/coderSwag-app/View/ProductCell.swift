69e22cc31dbcba1722a7a7cbb0f2ceda4453446a
SophieMBerger <berger.m.sophie@gmail.com>
2018-08-12 18:05:04 +0200

    Collection view added

---
+++ coderSwag-app/coderSwag-app/View/ProductCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCell.swift
+//  coderSwag-app
+//
+//  Created by Sophie Berger on 12.08.18.
+//  Copyright © 2018 SophieMBerger. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
